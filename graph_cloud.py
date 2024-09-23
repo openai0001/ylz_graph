edc324d01e1df2857a72366de0936c84452ba3c0
youht <youht@youhtdeMac-mini.local>
2024-09-23 18:54:36 +0800

    update

--- graph_cloud.py
+++ graph_cloud.py
@@ -2,6 +2,7 @@ from ylz_utils.config import Config
 from ylz_utils.database.neo4j import Neo4jLib
 from ylz_utils.langchain import LangchainLib
 from ylz_graph.graph1 import Graph1
+from ylz_graph.graph2 import Graph2
 
 Config.init('ylz_graph')
 langchainLib = LangchainLib()
@@ -14,4 +15,13 @@ def get_graph1():
     graph = graph1.get_graph()
     return graph
 
+def get_graph2():
+    print("graph_cloud:graph2")
+    graph2= Graph1(langchainLib)
+    graph2.set_thread("youht","default")
+    graph2.set_nodes_llm_config(("LLM.ZHIPU",None))
+    graph = graph2.get_graph()
+    return graph
+
 graph1 = get_graph1()
+graph2 = get_graph2()
---
+++ ylz_graph/graph2/__init__.py
@@ -0,0 +1,32 @@
+from ylz_utils.langchain.graph import GraphLib
+from langgraph.graph import StateGraph, START,END, MessagesState
+from langchain_core.messages import AIMessage,HumanMessage
+from langgraph.graph.state import CompiledStateGraph
+from langgraph.prebuilt import tools_condition
+
+class Graph2(GraphLib):
+    def __init__(self,langchainLib):
+        super().__init__(langchainLib)
+        self.set_nodes_llm_config()
+        self.set_websearch_tool('DUCKDUCKGO')
+        self.set_tools()
+        self.set_tools_executor()
+    def get_graph(self) -> CompiledStateGraph:
+
+        workflow = StateGraph(MessagesState)
+        workflow.add_node("robot",self.robot)
+        workflow.add_node("tools",self.tool_node)
+        workflow.add_edge(START,"robot")
+        workflow.add_conditional_edges("robot",tools_condition)
+        workflow.add_edge("tools","robot")        
+        graph = workflow.compile()
+        return graph
+
+    def human_action(self, graph, thread_id=None):
+        return super().human_action(graph, thread_id)
+    
+    async def robot(self,state:MessagesState):
+        self.llm = self.get_node_llm()
+        self.llm_bind_tools = self.llm.bind_tools(self.tools)
+        res  = await self.llm_bind_tools.ainvoke(state["messages"])
+        return {"messages":[res]}        
