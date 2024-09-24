1a7a984a98863cbf2e662f49e714b6c7706d0b75
youht <youht@youhtdeMac-mini.local>
2024-09-24 14:08:42 +0800

    update

--- ylz_graph/graph2/__init__.py
+++ ylz_graph/graph2/__init__.py
@@ -4,15 +4,29 @@ from langchain_core.messages import AIMessage,HumanMessage
 from langgraph.graph.state import CompiledStateGraph
 from langgraph.prebuilt import tools_condition,create_react_agent
 from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_community.utilities.tavily_search import TavilySearchAPIWrapper
+from langchain_core.runnables import RunnableLambda
+from langchain_core.tools import tool
+
+@tool
+def _wrapTavily(query):
+    '''
+    从互联网上查询query，返回字符串结果
+    '''
+    search = TavilySearchAPIWrapper(tavily_api_key="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")
+    tool = TavilySearchResults(api_wrapper=search,max_results=2)
+    tool_doc = tool | RunnableLambda(lambda doc:str(doc), name="Tavily2Document")
+    res = tool_doc.invoke(query)
+    return res
 
 class Graph2(GraphLib):
     def __init__(self,langchainLib):
         super().__init__(langchainLib)
         #self.set_websearch_tool('TAVILY')
-        tool = TavilySearchResults(max_results=2)
-        print("test tavily:",tool.invoke("What's a 'node' in LangGraph?"))
-        self.tools = [tool]
         #self.set_tools_executor(self.tools)
+        res = _wrapTavily("北京天气如何?")
+        print(res,type(res))
+        self.tools = [_wrapTavily]
         
     def get_graph(self) -> CompiledStateGraph:
         llm = self.get_node_llm()
