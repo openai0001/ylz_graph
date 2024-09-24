196e51a7a52e844bd56de1223f3a7b6db6a817cc
youht <youht@youhtdeMac-mini.local>
2024-09-24 09:33:05 +0800

    update

--- ylz_graph/graph2/__init__.py
+++ ylz_graph/graph2/__init__.py
@@ -2,7 +2,7 @@ from ylz_utils.langchain.graph import GraphLib
 from langgraph.graph import StateGraph, START,END, MessagesState
 from langchain_core.messages import AIMessage,HumanMessage
 from langgraph.graph.state import CompiledStateGraph
-from langgraph.prebuilt import tools_condition
+from langgraph.prebuilt import tools_condition,create_react_agent
 
 class Graph2(GraphLib):
     def __init__(self,langchainLib):
@@ -10,22 +10,23 @@ class Graph2(GraphLib):
         self.set_websearch_tool('TAVILY')
         self.tools = [self.websearch_tool]
         self.set_tools_executor(self.tools)
-    def get_graph(self) -> CompiledStateGraph:
+        self.llm = self.get_node_llm()
+        self.llm_bind_tools = self.llm.bind_tools(self.tools)
 
-        workflow = StateGraph(MessagesState)
-        workflow.add_node("robot",self.robot)
-        workflow.add_node("tools",self.tool_node)
-        workflow.add_edge(START,"robot")
-        workflow.add_conditional_edges("robot",tools_condition)
-        workflow.add_edge("tools","robot")        
-        graph = workflow.compile()
+    def get_graph(self) -> CompiledStateGraph:
+        graph = create_react_agent(self.llm,self.tools)
+        # workflow = StateGraph(MessagesState)
+        # workflow.add_node("robot",self.robot)
+        # workflow.add_node("tools",self.tool_node)
+        # workflow.add_edge(START,"robot")
+        # workflow.add_conditional_edges("robot",tools_condition)
+        # workflow.add_edge("tools","robot")        
+        # graph = workflow.compile(self.memory)
         return graph
 
     def human_action(self, graph, thread_id=None):
         return super().human_action(graph, thread_id)
     
     def robot(self,state:MessagesState):
-        self.llm = self.get_node_llm()
-        self.llm_bind_tools = self.llm.bind_tools(self.tools)
         res  = self.llm_bind_tools.invoke(state["messages"])
         return {"messages":[res]}        
