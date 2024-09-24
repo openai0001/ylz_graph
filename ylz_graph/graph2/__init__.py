41c25d781e3a45ea11d2179184815fd0c058b527
youht <youht@youhtdeMac-mini.local>
2024-09-24 14:46:45 +0800

    update

--- ylz_graph/graph2/__init__.py
+++ ylz_graph/graph2/__init__.py
@@ -2,11 +2,12 @@ from ylz_utils.langchain.graph import GraphLib
 from langgraph.graph import StateGraph, START,END, MessagesState
 from langchain_core.messages import AIMessage,HumanMessage
 from langgraph.graph.state import CompiledStateGraph
-from langgraph.prebuilt import tools_condition,create_react_agent
+from langgraph.prebuilt import ToolNode,tools_condition,create_react_agent
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_community.utilities.tavily_search import TavilySearchAPIWrapper
 from langchain_core.runnables import RunnableLambda
 from langchain_core.tools import tool
+from rich import print
 
 class Graph2(GraphLib):
     def __init__(self,langchainLib):
@@ -26,21 +27,23 @@ class Graph2(GraphLib):
         
     def get_graph(self) -> CompiledStateGraph:
         llm = self.get_node_llm()
-        print("[LLM]",llm)
+        self.llm_bind_tools = llm.bind_tools(self.tools)
+        print("[LLM]",self.llm_bind_tools)
         print("[TOOLS]",self.tools)
-        graph = create_react_agent(llm,self.tools)
-        # workflow = StateGraph(MessagesState)
-        # workflow.add_node("robot",self.robot)
-        # workflow.add_node("tools",self.tool_node)
-        # workflow.add_edge(START,"robot")
-        # workflow.add_conditional_edges("robot",tools_condition)
-        # workflow.add_edge("tools","robot")        
-        # graph = workflow.compile(self.memory)
+        #graph = create_react_agent(llm,self.tools)
+        workflow = StateGraph(MessagesState)
+        workflow.add_node("robot",self.robot)
+        workflow.add_node("tools",ToolNode(self.tools))
+        workflow.add_edge(START,"robot")
+        workflow.add_conditional_edges("robot",tools_condition)
+        workflow.add_edge("tools","robot")        
+        graph = workflow.compile(self.memory)
         return graph
 
     def human_action(self, graph, thread_id=None):
         return super().human_action(graph, thread_id)
     
     def robot(self,state:MessagesState):
+        print(state["messages"][-1])
         res  = self.llm_bind_tools.invoke(state["messages"])
         return {"messages":[res]}        
