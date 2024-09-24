4cc07a091fa3f29cc47ff394f3a28e46ca5a2738
youht <youht@youhtdeMac-mini.local>
2024-09-24 12:09:21 +0800

    update

--- ylz_graph/graph2/__init__.py
+++ ylz_graph/graph2/__init__.py
@@ -3,14 +3,17 @@ from langgraph.graph import StateGraph, START,END, MessagesState
 from langchain_core.messages import AIMessage,HumanMessage
 from langgraph.graph.state import CompiledStateGraph
 from langgraph.prebuilt import tools_condition,create_react_agent
+from langchain_community.tools.tavily_search import TavilySearchResults
 
 class Graph2(GraphLib):
     def __init__(self,langchainLib):
         super().__init__(langchainLib)
-        self.set_websearch_tool('TAVILY')
-        self.tools = [self.websearch_tool]
-        self.set_tools_executor(self.tools)
-
+        #self.set_websearch_tool('TAVILY')
+        tool = TavilySearchResults(max_results=2)
+        print("test tavily:",tool.invoke("What's a 'node' in LangGraph?"))
+        self.tools = [tool]
+        #self.set_tools_executor(self.tools)
+        
     def get_graph(self) -> CompiledStateGraph:
         llm = self.get_node_llm()
         print("[LLM]",llm)
